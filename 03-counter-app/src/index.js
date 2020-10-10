90bd824a72336ebb133c14ea21af812f5f9bc31f
Marcos <marcos_go2015@hotmail.com>
2020-10-10 20:19:44 -0300

    03-counter-app

---
+++ 03-counter-app/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp';
+
+import './index.css';
+
+
+const divRoot = document.querySelector('#root');
+
+
+ReactDOM.render(<CounterApp value={10}/>, divRoot)
+//ReactDOM.render( <PrimeraApp saludo="Hola mundo"/>, divRoot)
+
+//saludo="hola, soy goku" se crea la propiedad del componente
+
+
+
+
+
+
+
+
+
+
